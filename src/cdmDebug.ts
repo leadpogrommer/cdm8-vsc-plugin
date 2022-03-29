7227c4d64b5020cbf5baee2e0157f04c8ff00f48
leadpogrommer <ilyamerzlakov@gmail.com>
2022-03-29 22:01:30 +0700

    debugger support (only works on my machine)

---
+++ src/cdmDebug.ts
@@ -0,0 +1,254 @@
+import {
+    Logger,
+    logger,
+    LoggingDebugSession,
+    DebugSession,
+    InitializedEvent,
+    TerminatedEvent,
+    StoppedEvent,
+    BreakpointEvent,
+    OutputEvent,
+    ProgressStartEvent,
+    ProgressUpdateEvent,
+    ProgressEndEvent,
+    InvalidatedEvent,
+    Thread,
+    StackFrame,
+    Scope,
+    Source,
+    Handles,
+    Breakpoint,
+    MemoryEvent,
+    Variable,
+} from '@vscode/debugadapter';
+import { DebugProtocol } from '@vscode/debugprotocol';
+import { ChildProcess, exec, spawn } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+import { createInterface } from 'readline';
+import internal = require('stream');
+import WebSocket = require('ws');
+
+const assemblerPath = '/home/ilya/work/cdm8e/ORiGinalASM/assembler/main.py';
+const emulatorPath = '/home/ilya/work/cdm8e/ORiGinalASM/emulator/emulator.py';
+
+interface ILaunchRequestArguments extends DebugProtocol.LaunchRequestArguments {
+    program: string;
+}
+
+type CdmRegisterName = 'r0' | 'r1' | 'r2' | 'r3' | 'ps' | 'sp' | 'pc';
+
+interface ICdm8State {
+    registers: Record<CdmRegisterName, number>;
+    memory: number[];
+}
+
+type CdmRequest = CdmStateRequest | CdmStepRequest;
+
+interface CdmStateRequest {
+    action: 'state';
+    data: ICdm8State;
+}
+
+interface CdmStepRequest {
+    action: 'step';
+}
+
+interface CodeLocation {
+    file: string;
+    line: number;
+    column: number;
+}
+
+const registersVariableReference = 1;
+const memoryVariableReference = 2;
+const dataMemVariableReference = 3;
+
+export class CdmDebugSession extends DebugSession {
+    emulatorProcess!: ChildProcess;
+    socket!: WebSocket;
+    codeMap: Map<number, CodeLocation> = new Map();
+    latestState: ICdm8State | null = null;
+    static threadID = 1;
+
+    public constructor() {
+        super();
+
+        this.setDebuggerColumnsStartAt1(true);
+        this.setDebuggerLinesStartAt1(true);
+    }
+
+    protected initializeRequest(response: DebugProtocol.InitializeResponse, args: DebugProtocol.InitializeRequestArguments): void {
+        response.body = response.body || {};
+
+        // response.body.supVa
+        response.body.supportsConfigurationDoneRequest = true;
+        this.sendResponse(response);
+        this.sendEvent(new InitializedEvent());
+    }
+
+    protected async launchRequest(response: DebugProtocol.LaunchResponse, args: ILaunchRequestArguments) {
+        console.log(`launched, program = ${args.program}`);
+
+        const assemblerProcess = spawn('python', [assemblerPath, args.program]);
+        for await (const data of assemblerProcess.stdout) {
+            // console.log(data.toString());
+            this.sendEvent(new OutputEvent(data.toString(), 'debug console'));
+        }
+
+        const noExtensionPath = args.program.replace(new RegExp(`${path.extname(args.program)}$`), '');
+        const imgPath = noExtensionPath + '.img';
+        this.emulatorProcess = spawn('python', [emulatorPath, '--serve', imgPath]);
+
+        if (this.emulatorProcess.stdout === null || this.emulatorProcess.stderr === null) {
+            this.sendEvent(new TerminatedEvent());
+            return;
+        }
+
+        const emuInterface = createInterface(this.emulatorProcess.stdout);
+        let emuPort!: Number;
+
+        for await (const line of emuInterface) {
+            emuPort = parseInt(line);
+            break;
+        }
+        console.log(emuPort);
+        this.socket = new WebSocket(`ws://127.0.0.1:${emuPort}`);
+        this.socket.onmessage = (event) => {
+            this.onEmulatorMessage(JSON.parse(event.data.toString()));
+        };
+
+        this.sendResponse(response);
+        this.sendEvent(new StoppedEvent('entry', 1));
+
+        // TODO: check if we successfully loaded file
+        const codeMapFile = await fs.promises.open(noExtensionPath + '.dbg.json', 'r');
+        const codeMapJson = JSON.parse((await codeMapFile.readFile()).toString());
+        codeMapFile.close();
+
+        for (const [key, value] of Object.entries(codeMapJson)) {
+            this.codeMap.set(parseInt(key), value as CodeLocation);
+        }
+
+        console.log('done initialization');
+    }
+
+    private onEmulatorMessage(message: CdmRequest) {
+        if (message.action === 'state') {
+            // TODO: race condition: we need to wait for first state before finishing initialization
+            this.latestState = message.data;
+            if (this.codeMap.has(this.latestState.registers.pc)) {
+                console.log(this.codeMap.get(this.latestState.registers.pc));
+            }
+            console.log('got state');
+        }
+    }
+
+    private sendEmulatorMessage(msg: CdmRequest) {
+        this.socket.send(JSON.stringify(msg));
+    }
+
+    protected async stepInRequest(response: DebugProtocol.StepInResponse, args: DebugProtocol.StepInArguments, request?: DebugProtocol.Request) {
+        this.sendEmulatorMessage({ action: 'step' });
+
+        this.sendResponse(response);
+        this.sendEvent(new StoppedEvent('step', CdmDebugSession.threadID));
+    }
+
+    protected threadsRequest(response: DebugProtocol.ThreadsResponse): void {
+        console.log('thread request');
+        // runtime supports no threads so just return a default thread.
+        response.body = {
+            threads: [new Thread(CdmDebugSession.threadID, 'thread 1')],
+        };
+        this.sendResponse(response);
+    }
+
+    protected disconnectRequest(
+        response: DebugProtocol.DisconnectResponse,
+        args: DebugProtocol.DisconnectArguments,
+        request?: DebugProtocol.Request
+    ): void {
+        console.log('disconnect');
+        this.socket.close();
+        this.sendResponse(response);
+    }
+
+    // Requests go like this:
+    // stackTrace -> scopes -> variables
+    protected variablesRequest(
+        response: DebugProtocol.VariablesResponse,
+        args: DebugProtocol.VariablesArguments,
+        request?: DebugProtocol.Request
+    ): void {
+        if (this.latestState === null) {
+            this.sendResponse(response);
+            return;
+        }
+        console.log('variable request');
+        if (args.count === undefined || args.count === 0) {
+            args.count = Infinity;
+        }
+        if (args.start === undefined) {
+            args.start = 0;
+        }
+        response.body = { variables: [] };
+        if (args.variablesReference === registersVariableReference) {
+            for (const [name, value] of Object.entries(this.latestState.registers)) {
+                response.body.variables.push(new Variable(name, value.toString()));
+            }
+        } else if (args.variablesReference === memoryVariableReference) {
+            response.body.variables.push(new Variable('Data memory', '...', dataMemVariableReference));
+        } else if (args.variablesReference === dataMemVariableReference) {
+            for (let i = args.start; i < Math.min(this.latestState.memory.length, args.start + args.count); i++) {
+                response.body.variables.push(new Variable(`[${i.toString(16)}]`, this.latestState.memory[i].toString()));
+            }
+        }
+
+        this.sendResponse(response);
+    }
+
+    protected stackTraceRequest(
+        response: DebugProtocol.StackTraceResponse,
+        args: DebugProtocol.StackTraceArguments,
+        request?: DebugProtocol.Request
+    ): void {
+        // TODO: source reference
+        console.log('stack request');
+        response.body = { stackFrames: [] };
+        if (this.latestState !== null && this.codeMap.has(this.latestState.registers.pc)) {
+            const location = this.codeMap.get(this.latestState.registers.pc)!;
+
+            const frame = new StackFrame(0, 'frame', new Source(path.parse(location.file).base, location.file), location.line);
+            response.body.totalFrames = 1;
+            response.body.stackFrames.push(frame);
+        } else {
+            response.body.totalFrames = 0;
+            response.body.stackFrames = [];
+        }
+        this.sendResponse(response);
+    }
+
+    protected scopesRequest(response: DebugProtocol.ScopesResponse, args: DebugProtocol.ScopesArguments, request?: DebugProtocol.Request): void {
+        // TODO: do no hardcode frameID
+        if (args.frameId === 0) {
+            const registersScope = new Scope('Registers', registersVariableReference);
+            const memoryScope = new Scope('Memory', memoryVariableReference);
+            response.body = { scopes: [registersScope, memoryScope] };
+        }
+
+        this.sendResponse(response);
+    }
+
+    /**
+     * Called at the end of the configuration sequence.
+     * Indicates that all breakpoints etc. have been sent to the DA and that the 'launch' can start.
+     */
+    protected configurationDoneRequest(response: DebugProtocol.ConfigurationDoneResponse, args: DebugProtocol.ConfigurationDoneArguments): void {
+        super.configurationDoneRequest(response, args);
+
+        // notify the launchRequest that configuration has finished
+        // this._configurationDone.notify();
+        console.log('config done');
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,105 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { CancellationToken, DebugConfiguration, ProviderResult, WorkspaceFolder } from 'vscode';
+import { CdmDebugSession } from './cdmDebug';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cdm8-asm" is now active!');
+    // activate debug
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.cdm8.debugEditorContents', (resource: vscode.Uri) => {
+            let targetResource = resource;
+            if (!targetResource && vscode.window.activeTextEditor) {
+                targetResource = vscode.window.activeTextEditor.document.uri;
+            }
+            if (targetResource) {
+                vscode.debug.startDebugging(undefined, {
+                    type: 'cdm8',
+                    name: 'Run File',
+                    request: 'launch',
+                    program: targetResource.fsPath,
+                });
+            }
+        })
+    );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cdm8-asm.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from cdm8-asm!');
-	});
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.cdm8.getProgramName', (config) => {
+            return vscode.window.showInputBox({
+                placeHolder: 'Please enter the name of a .asm file in the workspace folder',
+                value: 'program.asm',
+            });
+        })
+    );
 
-	context.subscriptions.push(disposable);
+    // register debugger itself
+    const provider = new CdmConfigurationProvider();
+    context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('cdm8', provider));
+
+    context.subscriptions.push(
+        vscode.debug.registerDebugConfigurationProvider(
+            'cdm8',
+            {
+                provideDebugConfigurations(folder: WorkspaceFolder | undefined): ProviderResult<DebugConfiguration[]> {
+                    return [
+                        {
+                            name: 'Dynamic Launch',
+                            request: 'launch',
+                            type: 'cdm8',
+                            program: '${file}',
+                        },
+                    ];
+                },
+            },
+            vscode.DebugConfigurationProviderTriggerKind.Dynamic
+        )
+    );
+
+    const factory = new InlineDebugAdapterFactory();
+    context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('cdm8', factory));
+    if ('dispose' in factory) {
+        // i don't know ts
+        context.subscriptions.push(factory as unknown as { dispose(): any });
+    }
+}
+
+class CdmConfigurationProvider implements vscode.DebugConfigurationProvider {
+    /**
+     * Massage a debug configuration just before a debug session is being launched,
+     * e.g. add all missing attributes to the debug configuration.
+     */
+    resolveDebugConfiguration(
+        folder: WorkspaceFolder | undefined,
+        config: DebugConfiguration,
+        token?: CancellationToken
+    ): ProviderResult<DebugConfiguration> {
+        // if launch.json is missing or empty
+        if (!config.type && !config.request && !config.name) {
+            const editor = vscode.window.activeTextEditor;
+            if (editor && editor.document.languageId === 'cdm8asm') {
+                config.type = 'cdm8';
+                config.name = 'Launch';
+                config.request = 'launch';
+                config.program = '${file}';
+            }
+        }
+
+        if (!config.program) {
+            return vscode.window.showInformationMessage('Cannot find a program to debug').then((_) => {
+                return undefined; // abort launch
+            });
+        }
+
+        return config;
+    }
+}
+
+class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
+    createDebugAdapterDescriptor(_session: vscode.DebugSession): ProviderResult<vscode.DebugAdapterDescriptor> {
+        return new vscode.DebugAdapterInlineImplementation(new CdmDebugSession());
+    }
 }
 
 // this method is called when your extension is deactivated
